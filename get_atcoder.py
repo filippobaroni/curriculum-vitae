f25b23f35e69b2785aa2a35a67a653ffaaf2ca8f
Delfad0r <filippo.gianni.baroni@gmail.com>
2021-01-02 19:03:09 +0100

    sab 2 gen 2021, 19:03:09, CET

--- get_atcoder.py
+++ get_atcoder.py
@@ -2,13 +2,12 @@
 
 import requests
 import sys
-from bs4 import BeautifulSoup
+import re
 
 COLORS = {
     'user-yellow' : 'C0C000'
 }
 
 name = sys.argv[1]
-soup = BeautifulSoup(requests.get(f'http://atcoder.jp/user/{name}').content, features = 'html5lib')
-rank = soup.find('a', class_ = 'username').find('span')['class'][0]
+rank = re.search(r'<span class="(.*?)">' + name + '</span>', requests.get(f'http://atcoder.jp/user/{name}').text).group(1)
 sys.stdout.write(r'\textcolor[HTML]{%s}{%s}' % (COLORS[rank], name))
