a274d4dbde9d3559d3ed7f44ff6ddc6371686b88
Delfad0r <filippo.gianni.baroni@gmail.com>
2021-01-03 13:40:56 +0100

    dom 3 gen 2021, 13:40:56, CET

--- get_atcoder.py
+++ get_atcoder.py
@@ -1,13 +1,33 @@
 #! /usr/bin/env python3
 
+from collections import namedtuple
+import pickle
 import requests
 import sys
 import re
+import time
 
 COLORS = {
-    'user-yellow' : 'C0C000'
+    'user-yellow' : 'C0C000',
+    'user-red' : 'FF0000'
 }
 
+Cache = namedtuple('Cache', 'username time data')
+
 name = sys.argv[1]
-rank = re.search(r'<span class="(.*?)">' + name + '</span>', requests.get(f'http://atcoder.jp/user/{name}').text).group(1)
-sys.stdout.write(r'\textcolor[HTML]{%s}{%s}' % (COLORS[rank], name))
+try:
+    cache = pickle.load(open('atcoder.cache', 'rb'))
+    if cache.username != name or time.time() - cache.time > 3600:
+        cache = None
+except FileNotFoundError:
+    cache = None
+if cache is None:
+    try:
+        rank = re.search(r'<span class="(.*?)">' + name + '</span>', requests.get(f'http://atcoder.jp/user/{name}').text).group(1)
+        out = r'\textcolor[HTML]{%s}{%s}' % (COLORS[rank], name)
+        pickle.dump(Cache(name, time.time(), out), open('atcoder.cache', 'wb'))
+    except:
+        out = name
+else:
+    out = cache.data
+sys.stdout.write(out)
--- get_codeforces.py
+++ get_codeforces.py
@@ -1,16 +1,34 @@
 #! /usr/bin/env python3
 
+from collections import namedtuple
+import pickle
 import requests 
 import sys
+import time
 
 COLORS = {
     'international master' : 'FF8C00',
     'grandmaster' : 'FF0000'
 }
 
+Cache = namedtuple('Cache', 'username time data')
+
 name = sys.argv[1]
-r = requests.get(f'http://codeforces.com/api/user.info', params = {'handles' : name}).json()
-assert(r['status'] == 'OK')
-r = r['result'][0]
-sys.stdout.write(r'\textcolor[HTML]{%s}{%s}' % (COLORS[r['rank']], name))
-sys.exit(0)
+try:
+    cache = pickle.load(open('codeforces.cache', 'rb'))
+    if cache.username != name or time.time() - cache.time > 3600:
+        cache = None
+except FileNotFoundError:
+    cache = None
+if cache is None:
+    try:
+        r = requests.get('http://codeforces.com/api/user.info', params = {'handles' : name}).json()
+        assert(r['status'] == 'OK')
+        r = r['result'][0]
+        out = r'\textcolor[HTML]{%s}{%s}' % (COLORS[r['rank']], name)
+        pickle.dump(Cache(name, time.time(), out), open('codeforce.cache', 'wb'))
+    except:
+        out = name
+else:
+    out = cache.data
+sys.stdout.write(out)
